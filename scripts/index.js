ad4046c8d5002f109e691593e12430e4a88c5bad
Kevin <tohkev@gmail.com>
2022-01-14 19:22:27 -0800

    added scroll nav styles

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+const sections = document.querySelectorAll('.section-beginning');
+const navLi = document.querySelectorAll('.nav-item');
+
+window.addEventListener('scroll', () => {
+    let current;
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY + sectionHeight / 2 >= sectionTop) {
+            current = section.getAttribute('id');
+        }
+    })
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        if (li.classList.contains(current)) {
+            li.classList.add('active');
+        }
+    })
+})
